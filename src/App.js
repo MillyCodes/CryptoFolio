c3aac4b7308025546ca808d191f0fb02a36a3fd6
Milly Rodriguez <millz118@gmail.com>
2018-11-12 11:51:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+var NumberFormat = require("react-number-format");
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cryptos: []
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get(
+                "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,VEN,PRL,SUB,DASH&tsyms=USD,BTC"
+            )
+
+            .then(res => {
+                const cryptos = res.data;
+                console.log(cryptos);
+                this.setState({ cryptos: cryptos });
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {Object.keys(this.state.cryptos).map(key => (
+                    <div key={key.toString()} id="crypto-container">
+                        <span className="left">{key}</span>
+                        <span className="right">
+                            <NumberFormat
+                                value={this.state.cryptos[key].USD}
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"$"}
+                            />
+                        </span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
